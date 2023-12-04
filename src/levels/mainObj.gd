c2112f16e06ec7d887a8d8aa96fa490653649699
joao-zip <oliveira.joao@unifesp.br>
2023-12-04 14:53:47 -0300

    refactor: adição do script principal para o jogo

---
+++ src/levels/mainObj.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var win : bool
+var init_position : Vector2
+
+func _ready():
+	init_position = global_position
+	win = false
+
+
+func _process(delta):
+	if Input.is_action_pressed("ui_left_click"):
+		global_position = get_global_mouse_position()
+	else:
+		if not win:
+			global_position = init_position
+
+
+func _on_sombra_1_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print('Ganhemo')
