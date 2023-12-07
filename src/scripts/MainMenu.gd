6d0f13d955863fbc802877a19729600d9de57bfd
joao-zip <oliveira.joao@unifesp.br>
2023-12-07 11:14:39 -0300

    added: tela de niveis

--- src/scripts/MainMenu.gd
+++ src/scripts/MainMenu.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+export var levelScene : PackedScene
 
 func _init() -> void:
 	var screen_size: Vector2 = OS.get_screen_size()
 	var window_size: Vector2 = OS.get_window_size()
 	
 	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)
+
+
+func _on_Jogar_button_up():
+	get_tree().change_scene(levelScene.resource_path)
