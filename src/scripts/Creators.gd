9526f0adfb600e67b3e1acace763a89560bc4af1
joao-zip <oliveira.joao@unifesp.br>
2023-12-08 11:33:01 -0300

    added: transicao entre as cenas

--- src/scripts/Creators.gd
+++ src/scripts/Creators.gd
@@ -4,11 +4,7 @@ extends Node2D
 
 func _ready():
 	pass
-
-
-#func _process(delta):
-#	pass
-
+	
 
 func _on_Voltar_button_up():
-	get_tree().change_scene("res://MainMenu.tscn")
+	SceneTransition.change_scene("res://MainMenu.tscn")
--- src/scripts/Levels.gd
+++ src/scripts/Levels.gd
@@ -2,28 +2,28 @@ extends Node2D
 
 
 func _on_level_1_button_up():
-	get_tree().change_scene("res://levels/level_1/level_1.tscn")
+	SceneTransition.change_scene("res://levels/level_1/level_1.tscn")
 
 
 func _on_Voltar_button_up():
-	get_tree().change_scene("res://MainMenu.tscn")
+	SceneTransition.change_scene("res://MainMenu.tscn")
 
 
 func _on_level_2_button_up():
-	get_tree().change_scene("res://levels/level_2/level_2.tscn")
+	SceneTransition.change_scene("res://levels/level_2/level_2.tscn")
 
 
 func _on_level_3_button_up():
-	get_tree().change_scene("res://levels/level_3/level_3.tscn")
+	SceneTransition.change_scene("res://levels/level_3/level_3.tscn")
 
 
 func _on_level_5_button_up():
-	get_tree().change_scene("res://levels/level_5/level_5.tscn")
+	SceneTransition.change_scene("res://levels/level_5/level_5.tscn")
 	
 	
 func _on_level_7_button_up():
-	get_tree().change_scene("res://levels/level_7/level_7.tscn")
+	SceneTransition.change_scene("res://levels/level_7/level_7.tscn")
 
 
 func _on_level_9_button_up():
-	get_tree().change_scene("res://levels/level_9/level_9.tscn")
+	SceneTransition.change_scene("res://levels/level_9/level_9.tscn")
