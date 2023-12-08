7b4d08ee6b46ed0f0669b8786b4806d8e490f1fa
matheus <matheus.resteves@gmail.com>
2023-12-08 14:30:17 -0300

    added: tudo ai

---
+++ src/scripts/Game_over.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
+
+
+func _on_tentar_btn_pressed():
+	var current_scene = get_tree().get_current_scene()
+	get_tree().change_scene(current_scene)
