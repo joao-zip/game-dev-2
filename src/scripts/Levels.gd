21b9f11a47c4027e792e6f4fc1537348e624dab1
joao-zip <oliveira.joao@unifesp.br>
2023-12-08 11:16:11 -0300

    added: niveis impares na cena de niveis

--- src/scripts/Levels.gd
+++ src/scripts/Levels.gd
@@ -15,3 +15,15 @@ func _on_level_2_button_up():
 
 func _on_level_3_button_up():
 	get_tree().change_scene("res://levels/level_3/level_3.tscn")
+
+
+func _on_level_5_button_up():
+	get_tree().change_scene("res://levels/level_5/level_5.tscn")
+	
+	
+func _on_level_7_button_up():
+	get_tree().change_scene("res://levels/level_7/level_7.tscn")
+
+
+func _on_level_9_button_up():
+	get_tree().change_scene("res://levels/level_9/level_9.tscn")
